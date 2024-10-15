d90cc5f0541777ac49a24f53e00b6322cd99c8e4
LeJin27 <jiqle@ucsc.edu>
2024-10-15 15:56:03 -0700

    test

--- database.py
+++ database.py
@@ -5,16 +5,23 @@ import psycopg2
 Used to create a data for all todo related functions 
 
 create_table 
-create_task  //create task given properties 
-drop_task //drop tasks with a given id
 ---
 Different queries
 ---
-get_task_done
-get_task_not_done
+update_task
+create_task  
+delete_task 
+read_task_done
+read_task_not_done
 """
 
 class TaskDatabase:
+    
+
+
+
+
+
     # Use localhost, minmax, postgres, your password, 5432 
     def __init__(self, host, dbname, user, password, port):
         """
@@ -32,7 +39,6 @@ class TaskDatabase:
             self.connection = psycopg2.connect(host=self.host, dbname = self.dbname, user=self.user, password=self.password, port = self.port)
             self.cursor = self.connection.cursor()
             print(f"Database '{self.dbname}' found and connected.")
-        
         except Exception as e:
             if "does not exist" in str(e):
                 self.create_database()
@@ -40,13 +46,12 @@ class TaskDatabase:
                 print("Something really went wrong")
         
 
-        # Creating table
+        # Creating Table of Tasks
         print("Creating tasks table")
         try:
             self.cursor.execute("CREATE TABLE IF NOT EXISTS tasks (task_id SERIAL PRIMARY KEY, task_desc VARCHAR(255), task_is_open BOOLEAN);")
         except Exception as e:
             print("Something when wrong")
-
         self.connection.commit()
     
 
@@ -72,7 +77,7 @@ class TaskDatabase:
         
 
 
-test = TaskDatabase("localhost", "minmax", "postgres", "dog", 5432)
+test = TaskDatabase("localhost", "matthew", "postgres", "dog", 5432)
 
 
 
