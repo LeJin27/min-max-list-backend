8e7505c070073bae62eb9b3dc6db3a3373aa75d4
psam1804 <psamudra@ucsc.edu>
2024-10-22 18:07:15 -0700

    fixed

---
+++ login.py
@@ -0,0 +1,6 @@
+import firebase_admin
+from firebase_admin import credentials, auth
+
+# Path to your Firebase private key file
+#cred = credentials.Certificate()
+#firebase_admin.initialize_app(cred)
